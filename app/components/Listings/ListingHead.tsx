e9fd9316e885749e4f587eb899b5bff586447c77
kmdshojib <kmd.shojib@gmail.com>
2023-06-22 16:27:33 +0600

    individual listing page has been created

---
+++ app/components/Listings/ListingHead.tsx
@@ -0,0 +1,43 @@
+"use client"
+import useCountries from '@/app/hooks/useCountries';
+import { SafeUser } from '@/app/types';
+import React from 'react'
+import Heading from '../Heading';
+import Image from 'next/image';
+import HeartButton from '../HeartBution';
+
+interface ListingHeadProps {
+    title: string;
+    locationValue: string;
+    imageSrc: string;
+    id: string;
+    currentUser?: SafeUser | null;
+}
+
+const ListingHead: React.FC<ListingHeadProps> = ({
+    title, locationValue, imageSrc, id, currentUser
+}) => {
+    const { getByValues } = useCountries();
+    const location = getByValues(locationValue)
+    return (
+        <div>
+            <Heading
+                title={title}
+                subTitle={`${location?.region}, ${location?.lable}`}
+            />
+            <div className='w-full h-[60vh] overflow-hidden rounded-xl relative'>
+                <Image
+                    alt="Iamge"
+                    src={imageSrc}
+                    fill
+                    className='object-cover w-full mt-5'
+                />
+                <div className="absolute top-5 right-5">
+                    <HeartButton listingId={id} currentUser={currentUser} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ListingHead
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,9 +1,11 @@
 "use client"
+import React, { useMemo } from 'react'
 import Container from '@/app/components/Container';
-import {categories} from '@/app/components/NavBar/Categories';
+import { categories } from '@/app/components/NavBar/Categories';
 import { SafeListing, SafeUser } from '@/app/types';
 import { Reservation } from '@prisma/client';
-import React, { useMemo } from 'react'
+import ListingHead from './../../components/Listings/ListingHead';
+import ListingInfo from '@/app/components/Listings/ListingInfo';
 
 interface ListingClientProps {
     reservations?: Reservation[];
@@ -19,20 +21,33 @@ const ListingClient: React.FC<ListingClientProps> = ({
     const category = useMemo(() => {
         return categories.find((item: any) => item.label === listing.category)
     }, [listing.category])
+    const { description, user, roomCount, guestCount, bathRoomCount, loactionValue } = listing
     return (
-       <Container>
+        <Container>
             <div className="max-w-screen-lg mx-auto">
                 <div className="flex flex-col gap-6">
-                    <ListingHead 
+                    <ListingHead
                         title={listing.title}
                         imageSrc={listing.imageSrc}
                         locationValue={listing.loactionValue}
                         id={listing.id}
                         currentUser={currentUser}
                     />
+                    <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+                        <ListingInfo
+                            user={user}
+                            category={category}
+                            description={description}
+                            roomCount={roomCount}
+                            guestCount={guestCount}
+                            bathroomCount={bathRoomCount}
+                            locationValue={loactionValue}
+
+                        />
+                    </div>
                 </div>
             </div>
-       </Container>
+        </Container>
     )
 }
 
